d903a750387cee7df870ff5ae6554461ba228c06
Jimish Fotariya <fotariyajimish@gmail.com>
2016-08-13 14:55:19 +0530

    key matrix k1,k2,k3

--- index.c
+++ index.c
@@ -5,11 +5,43 @@ char k1[] = "66FF3CE3491C5EDA";
 char k2[] = "95EFFBE191E22DB4";
 char k3[] = "9CC98A29456677A6";
 
+int key1[4][4], key2[4][4], key3[4][4];
 int zigZagIndex[4][4];
+
+char plainText[] = "ASDF48723498";
 
 int main(int argc, char const *argv[])
 {	
-	int i, j, m, n, ti, tj, zi;
+	int i, j, m, n, ti, tj, ix1, ix2, ix3, ix4, zi;
+
+	m = 4;
+/**
+ *
+ * KEY EXPANSION
+ *
+ */
+
+	 /**
+	  * Derive A-z indexes for keys 
+	  * a = 0, B = 1 ... z = 25
+	  *	1 = 1 , 2 = 2, 0 = 0 
+	  */
+	 
+
+		for( i = 0; k1[i] != '\0' ; i++ ){
+			
+			/**
+			 * 1D 1x16 marrix to 2D 4x4 
+			 */
+				
+				ti = i / 4;
+				tj = i % 4;
+
+				key1 [ti][tj] = toAZindex( k1[i] );
+				key2 [ti][tj] = toAZindex( k2[i] );
+				key3 [ti][tj] = toAZindex( k3[i] );
+		}
+
 
 	/**
 	 *
