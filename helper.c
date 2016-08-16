3ac5639e12028ba5135eed54c27559737130ca95
Jimish Fotariya <fotariyajimish@gmail.com>
2016-08-16 14:53:08 +0530

    decryption

--- helper.c
+++ helper.c
@@ -1,16 +1,15 @@
 int toAZindex(char c)
 {
 
- 	if(c >= 'a' && c <= 'z')
+    if(c >= 'A' && c <= 'Z')
+    {   
+        c = c - 65;   
+    }
+    else if(c >= 'a' && c <= 'z')
     {
-     	c = c - 97;
-    }
-    else if(c >= 'A' && c <= 'Z')
-    {	
-		c = c - 65;   
-    }
-    else if(c >= '1' && c <='9' ){
-		c = c-48;   
+        c = c - 97;
+    }else if(c > 47 && c < 58 ){
+        c = c - 48;   
     }
 
     return c;
@@ -51,12 +50,80 @@ void copy4x4( int a[4][4], int b[4][4]){
 
 void showInt4x4(int tmp[4][4]){
         int i,j;
+        printf("\n");
         for ( i = 0; i < 4; i++)
         {
             for ( j = 0; j < 4; j++)
             {
                 printf("%d\t", tmp[i][j]);
             }
-            printf("\n");
+        printf("\n");
         }
 }
+
+void showChar4x4(int tmp[4][4], int charStatus[4][4] ){
+        int i,j;
+        for ( i = 0; i < 4; i++)
+        {
+            for ( j = 0; j < 4; j++)
+            {
+                if( charStatus[i][j] ){
+                    printf("%c \t", toAZChar( tmp[i][j]) );
+                }else{
+                    printf("%d\t",  tmp[i][j] );
+                }
+
+            }
+        printf("\n");
+        }
+}
+
+
+
+void displayKeys( int key[34][4][4] ){
+
+        int i,k;
+        for (int i = 1; i <= 3; ++i)
+        {
+            k = (i*10);
+
+            printf("\n= = = = = = = = = = = = = = = = = = = = = = \n-- Key %d \n",i);
+            showInt4x4( key[ i ] );
+            
+            printf("\n----- Expanded Keys of %d \n",i);
+
+            showInt4x4( key[ k ] );
+            showInt4x4( key[ k+1 ] );
+            showInt4x4( key[ k+2 ] );
+            showInt4x4( key[ k+3 ] );
+        
+        }
+
+}
+
+
+void showChar4x4InSequence(int tmp[4][4], int charStatus[4][4] ){
+        int i,j;
+        printf("\n= = = = = = = = = DECRYPTED MESSAGE = = = = = = = = = = = = \n"   );
+
+        for ( i = 0; i < 4; i++)
+        {
+            for ( j = 0; j < 4; j++)
+            {
+                if( tmp[i][j] == 0 &&  charStatus[i][j] != 1  ){
+                    continue;
+                }
+                if( charStatus[i][j] ){
+                    printf("%c", toAZChar( tmp[i][j]) );
+                }else{
+                    printf("%d",  tmp[i][j] );
+                }
+
+            }
+        }
+        printf("\n \n"   );
+
+}
+
+
+
